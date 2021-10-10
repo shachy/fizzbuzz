64dcf00f3300e32438e4f7feff919d4f3cf9e935
shachi <52762781+shachy@users.noreply.github.com>
2021-10-10 18:26:36 +0300

    Update fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -2,14 +2,9 @@
 
 void main() {
 	for(int i = 1; i < 100; i++) {
-		if(i%15 == 0) {
-			printf("fizzbuzz")
-		} else if(i%3 == 0) {
-			printf("fizz")
-		} else if(i%5 == 0) {
-			printf("buzz")
-		} else {
-			printf("%d", i)
-		}
+		if(i%15 == 0) { printf("fizzbuzz") }
+		else if(i%3 == 0) { printf("fizz") }
+		else if(i%5 == 0) { printf("buzz") }
+		else { printf("%d", i) }
 	} 
 }
